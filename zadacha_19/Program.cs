fd888d50ce629ca6f82dcae19fe7b91ed0dabd9d
Apslll <whiteswan104@bk.ru>
2023-01-15 15:41:30 +0300

    задача19

---
+++ zadacha_19/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 19 
+
+// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет,
+// является ли оно палиндромом. Через строку нельзя решать само собой.
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while (index < x) index = index * 10;
+
+// Console.WriteLine(index);
+
+if (Check(x, index)) Console.WriteLine($"Заданное число: {x} - палиндром");
+else Console.WriteLine($"Заданное число: {x} - не палиндром");
+
+Boolean Check(int x, int index)
+{
+    int count = 1;
+    Boolean result = false;
+    while (count < index)   
+    {
+        if ((((x % (index / count)) / (index / (10 * count)))) == ((x % (10 * count)) / count)) result = true;
+        else
+        {
+            result = false;
+            break;
+        }
+        count = count * 10;
+    }
+    return result;
+}
